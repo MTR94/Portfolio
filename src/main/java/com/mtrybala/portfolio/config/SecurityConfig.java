b094a02180e7dca61e179375ee99e7eb39170840
Marcin Trybała <trybala.mar@gmail.com>
2024-02-12 20:35:43 +0100

    Konfiguracja klasy SecurityConfig

--- src/main/java/com/mtrybala/portfolio/config/SecurityConfig.java
+++ src/main/java/com/mtrybala/portfolio/config/SecurityConfig.java
@@ -1,26 +1,27 @@
 package com.mtrybala.portfolio.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(authorize -> authorize
-                        .requestMatchers("/public/**").permitAll()
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/projects").permitAll()
+                        .requestMatchers("/projects/config").hasRole("ADMIN")
                         .anyRequest().authenticated())
-                ;
+                .httpBasic(withDefaults());
         return http.build();
     }
-
-
 }
-
